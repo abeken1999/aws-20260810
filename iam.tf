1b8b0905aeb7b55ff16505f7fa309ed2a8b34df2
abeken1999 <abeken.manage@gmail.com>
2026-08-17 23:47:34 +0900

    fix: update oidc configuration for github actions

--- iam.tf
+++ iam.tf
@@ -2,6 +2,7 @@
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
+  # GitHub の OIDC 証明書サムプリント（最新のルート証明書指紋）
   thumbprint_list = [
     "6938fd4d98bab03faadb97b34396831e3780aea1",
     "1c3d215b070979b1a110759ff1183b263a4658a3"
@@ -22,6 +23,9 @@ resource "aws_iam_role" "github_actions" {
           Federated = aws_iam_openid_connect_provider.github.arn
         }
         Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
           StringLike = {
             "token.actions.githubusercontent.com:sub" = "repo:abeken1999/aws-20260810:*"
           }
